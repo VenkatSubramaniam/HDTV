4d888ebed034c08d5ba335a80faafd21beec31ba
Venkat <dragzarmilon@gmail.com>
2019-10-17 18:58:52 -0400

    text file generator

---
+++ gen_txt.py
@@ -0,0 +1,20 @@
+import argparse
+import random
+
+def main(delimiter, num_columns, num_rows):
+    vars = ["", ".", "foo", "bar", "trolls", "lol", "dayum", "5", "7"]
+    with open("test_file.txt", "w+") as f:
+        for j in range(num_rows):
+            for i in range(num_columns):
+                f.write(f"{vars[random.randint(0,len(vars)-1)] + delimiter}")
+            f.write("\n")
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--delimiter", type=str, dest='delimiter')
+    parser.add_argument("--num_columns", type=int, dest='nc')
+    parser.add_argument("--num_rows", type=int, dest='nr')
+    args = vars(parser.parse_args())
+    main(args['delimiter'], args['nc'], args['nr'])
+
