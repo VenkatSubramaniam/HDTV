ccdea8fd23b15425a59b9a16adb2b0a009f7bffd
Trijeet Sethi <trijeets@gmail.com>
2019-10-22 21:19:27 -0400

    test instantiate and QoLs in ingester

--- parser/xml/tests/test_xml_parser.py
+++ parser/xml/tests/test_xml_parser.py
@@ -1,8 +1,15 @@
 import pytest
 
+##Connecting to postgres:
+def test_pg_connection:
+	parser = ingester(uname="username", pword="password", db="postgres")
+	parser.validate_login()
+
+
 ##No columns specified
 def test_xml_return:
-	pass
+	parser = ingester(fname="dummy.xml", cols="yes", uname="username", pword="password", unit="book")
+    parser.streaming()
 
 ##Some columns specified
 def test_partial_xml_return:
