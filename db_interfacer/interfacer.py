f1497c42b875943931bdcb8101cc959539996fae
Venkat <dragzarmilon@gmail.com>
2019-11-07 21:51:24 -0500

    adding params to dbinter

--- db_interfacer/interfacer.py
+++ db_interfacer/interfacer.py
@@ -4,13 +4,13 @@ import psycopg2 as pg # kill me this name
 
 class DBInterfacer:
 
-    def __init__(self) -> None:
-        self.connection, self.cursor = self._establish_postgres_connection()
+    def __init__(self, uname: str, db: str, port: str) -> None:
+        self.connection, self.cursor = self._establish_postgres_connection(uname, db, port)
 
     @staticmethod
-    def _establish_postgres_connection() -> None:
+    def _establish_postgres_connection(uname: str, db: str, port: str) -> None:
         try:
-            connection = pg.connect(user="postgres", port="5432", database=db)  # do we have to create this before? The db, I mean.
+            connection = pg.connect(user=uname, port=port, database=db)  # do we have to create this before? The db, I mean.
             return connection, connection.cursor()
         except (Exception, psycopg2.Error) as error:
             raise Exception("We were unsuccessful in connecting to postgres. Are you sure you set up your database as requested?")  # think this works? We shall see.
