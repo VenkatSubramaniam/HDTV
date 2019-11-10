39faef436e19cfaca0ec33f2f1956c2614a284c5
Trijeet Sethi <trijeets@gmail.com>
2019-11-10 02:02:33 -0500

    added explicit typing

--- parser/ingester.py
+++ parser/ingester.py
@@ -8,7 +8,7 @@ import operator
 import pytest
 import sys
 import time
-# from typing import
+from typing import Dict, List
 
 #Internals
 #from json_parser import jparser
@@ -18,18 +18,20 @@ from parser.xml_parser import lumberjack
 ## Main ingestion object:
 class Ingester:
     """take file and column labels and insert into postgresql"""
-    def __init__(self, fname, ftype="xml", interface=None, cols=None, unit=None, validation_file=None, repeats={"keys":False}):
+    def __init__(self, fname: str, ftype="xml": str, interface=None: object, cols=None: List, unit=None: List, validation_file=None: str, repeats={"keys":False}: Dict[str,bool]) -> None:
         self.interface = interface #expects the interface object
-        self.filename = fname #expects a path
-        self.columns = cols #expects a list of names (str)
-        self.validation_file = validation_file #expects some DTD
         self.repeats = repeats
         # self.tree = None
+        self.filename = fname #expects a path
+        self.columns = cols #expects a list of names (str)
+        self.unit = unit #expects a list of main units
+        self.validation_file = validation_file #expects some DTD
+        ##        
         self.filetype = ftype
         if self.filetype=="lxml":
-            lumberjack = Lumberjack(fname=self.fname, interface=self.interface, cols=self.cols, validation_file=self.validation_file, repeats=self.repeats)
-            lumberjack.get_tree()
-            lumberjack.write_stream(self)
+            Lumberjack = lumberjack(fname=self.fname, interface=self.interface, cols=self.cols, validation_file=self.validation_file, repeats=self.repeats)
+            Lumberjack.get_tree()
+            Lumberjack.write_stream(self)
         #encoding specification from learner
         #host specification
         
@@ -39,7 +41,7 @@ class Ingester:
     unit = property(operator.attrgetter('_unit'))
     
     @filename.setter
-    def filename(self, f):
+    def filename(self, f: str) -> None:
         if not os.path.isfile(os.path.join(os.getcwd(),f)):
             #default behavior is for the tool to be called in the directory of the file.
             raise Exception("No file at given path")
@@ -47,7 +49,7 @@ class Ingester:
             self._filename = f       
         
     @columns.setter
-    def columns(self, c):
+    def columns(self, c: List) -> None:
         if not c:
             self._columns = False
         else:
@@ -58,7 +60,7 @@ class Ingester:
                 assert len(self._columns)==1, "pass a list of names or single string"
    
     @validation_file.setter
-    def validation_file(self, vf):
+    def validation_file(self, vf: str) -> None:
         if not vf:
             self._validation_file = False
         else:
@@ -69,8 +71,11 @@ class Ingester:
                 self._validation_file = vf
             
     @unit.setter
-    def unit(self, unit):
-        if unit:
-            #infer this later (for each set(base element) in the tree, proceed)
-            assert type(unit)==str, "primary unit must be string" 
-        self._unit = unit
+    def unit(self, unit: List) -> None:
+        if type(unit)!=list:
+            if unit:
+                assert type(unit)==str, "units must be string" 
+                self.unit = list(unit)
+        else:
+            assert type(unit[0])==str, "units must be string" 
+            self._unit = unit
