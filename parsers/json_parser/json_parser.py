0c0f57502143b55d55a3217bee6e6f2c51f8bbe9
Jianfei Bi <bjf_0818@yahoo.com>
2019-12-06 01:38:35 -0500

    Revised json parser

--- parsers/json_parser/json_parser.py
+++ parsers/json_parser/json_parser.py
@@ -1,59 +1,38 @@
 import json
 
 class json_parser:
-	def __init__(self, fname, interface = None, cols = None, batchsize, validation_file = None, repeats = None):
+	def __init__(self, fname, interface = None, cols = None, batchsize, table_name):
 		self.filename = fname
-		self.interface = interface
+		self.db_inter = interface
 		self.cols = cols # expects a list of desired column names
 		self.batchsize = batchsize # The size of passes in one commit.
-		self.validation_file = validation_file # Do we need DTD for json?
-		self.repeats = repeats # record whether there exist repeated columns.
+		self.table_name = table_name
 
 	# To convert json file to a python dictionary for easy access.
 	def get_dict(self):
 		try:
-			self.dict = json.loads(self.filename, indent = 2, sort_keys = False)
+			with open(self.filename,  "r") as f:
+				self.dict = json.loads(f.read())
 		except:
 			print("Failed to convert json file to python dict!")
 			pass
 
-	# Get all the column names.
-	def get_leaves(item, key = None):
-		item = self.dict
-	    if isinstance(item, dict):
-	        leaves = {}
-	        for i in item.keys():
-				"""
-				Having trouble dealing with nested json here.
-				"""
-	            leaves.update(get_leaves(item[i], i))
-	        return leaves
-	    elif isinstance(item, list):
-	        leaves = {}
-	        for i in item:
-	            leaves.update(get_leaves(i, key))
-	        return leaves
-	    else:
-	        return {key : item}
+	def get_struct(self):
+		# Use a set to keep the attribute names.
+		self.attr = set()
+		# Use a list of tuples to store each tuple.
+		self.val = []
 
-	def parse_file(self):
-		with open(self.filename) as f:
-		    json_data = json.loads(f.read())
-		    #json_data = json.load(f_input)['LOG_28MAY']
-
-		# First parse all entries to get the complete fieldname list
-		fieldnames = set()
-
-		for entry in json_data:
-		    fieldnames.update(get_leaves(entry).keys())
-
-		return fieldnames
-
+		for i in self.dict:
+		    for item in self.dict[i]:
+		        for k in item.keys():
+		            self.attr.add(k)
+		        self.val.append(tuple(item.values()))
 
 	# Make sure all the columns requested by user are valid( exist in file).
-	def validate_specified_cols(self, fieldnames):
+	def validate_specified_cols(self, self.attr):
 		for col in self.cols:
-			if col in fieldnames:
+			if col in attr:
 				pass
 			else:
 				raise ValueError(col)
@@ -61,14 +40,13 @@ class json_parser:
 
 	def write_stream(self):
 		counter = 0
-		with open(self.filename) as f:
-		    json_data = json.loads(f.read())
+		batchsize = 1000
 
 		# If we have columns specified.
 		if self.cols:
 			for field in self.cols:
 				query = {}
-				query[field].qppend(json_data[field])
+				query[field].append(self.dict[field])
 			self.db_inter.insert_row(table_name, query)
 			counter += 1
 
@@ -80,7 +58,7 @@ class json_parser:
 		else:
 			for field in fieldnames:
 				query = {}
-				query[field].qppend(json_data[field])
+				query[field].append(self.dict[field])
 			self.db_inter.insert_row(table_name, query)
 			counter += 1
 
