e612ed266e35ccf91516190eef8be950cce77570
Trijeet Sethi <trijeets@gmail.com>
2019-12-10 10:41:17 -0500

    actual functional graphs - new full timing system

---
+++ integration_tests/full_project_timing_test.py
@@ -0,0 +1,28 @@
+import os
+import pickle
+import psycopg2
+import timeit
+
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="")
+conn.set_isolation_level(0)
+cur = conn.cursor()
+
+
+vals = [1e2,5e2,1e3,5e3,2e4,5e4,75e3,1e5,2e5,25e4,3e5,35e4,45e4,5e5,6e5,75e4,1e6,15e5,2e6,5e6,1e7]
+file_map = {key: f"{key}_test.csv" for key in vals}
+
+t_d = {k:[] for k in vals}
+for n in vals:
+	for i in range(30):
+		t0 = timeit.default_timer()
+		os.chdir("../")
+		print(file_map[n])
+		os.system(f"python veranda.py -f ./util/{file_map[n]} -c appln_filing_date, appln_filing_year, appln_id, appln_kind -U postgres -D postgres -P password -p 5432")
+		t_d[n].append(timeit.default_timer() - t0)
+		print(t_d)
+		#Try with just the single drop existing
+		cur.execute("""DROP TABLE IF EXISTS PEOPLE;""")
+		break
+	break
+with open('./util/full_project_dump.pickle', 'wb') as handle:
+		pickle.dump(t_d,handle,protocol=pickle.HIGHEST_PROTOCOL)
